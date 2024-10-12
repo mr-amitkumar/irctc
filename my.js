d5b08f801ac3273d4e13b549e315bc95c84d12ae
mr-amitkumar <amitkumarmahapatra200@gmail.com>
2024-10-12 10:16:17 +0530

    Initial commit

---
+++ my.js
@@ -0,0 +1,13 @@
+function display(){
+    var l = document.getElementById("login").value;
+var p = document.getElementById("pass").value;
+
+if(l == ""){
+    window.alert("enter vaild id");
+    return 0;
+}
+if(p == ""){
+    window.alert("Enter your password");
+    return 0;
+}
+}
